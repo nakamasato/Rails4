d64949a99304413cd65035bcd9e214cc65974b82
MasatoNaka <utjxs.ntn@hotmail.co.jp>
2013-10-02 22:09:26 +0800

    erb2haml brypt

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,6 @@
 module HomeHelper
+  def practice_link
+    logger.debug "saaaa"
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails4::Application.routes.draw do
   get "home/index"
+  get "home/practice_link"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
